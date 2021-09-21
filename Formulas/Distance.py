6695c8f19f956714a5e04ec603abe1fbd8f61a6a
Statheros Dimitris <71936023+Mitsos03@users.noreply.github.com>
2021-09-21 10:41:45 +0300

    Create Distance.py

---
+++ Formulas/Distance.py
@@ -0,0 +1,250 @@
+def mili_cm():
+    mili = (float(input(Fore.GREEN+"Give mm value: ")))
+    cm = mili/10
+    print(cm, "Cm")
+
+
+def cm_mili():
+    cm = (float(input(Fore.GREEN+"Give cm value: ")))
+    mili = cm*10
+    print(mili, "Mm")
+
+
+def mili_meter():
+    mili = (float(input(Fore.GREEN+"Give mm value: ")))
+    metre = mili/1000
+    print(metre, "M")
+
+
+def meter_mili():
+    metre = (float(input(Fore.GREEN+"Give m value: ")))
+    mili = metre*1000
+    print(mili, "Mm")
+
+
+def cm_meter():
+    cm = (float(input(Fore.GREEN+"Give cm value: ")))
+    metre = cm/100
+    print(metre, "M")
+
+
+def meter_cm():
+    metre = (float(input(Fore.GREEN+"Give m value: ")))
+    cm = metre*100
+    print(cm, "Cm")
+
+
+def mili_km():
+    mili = (float(input(Fore.GREEN+"Give mm value: ")))
+    km = mili/100000
+    print(km, "Km")
+
+
+def km_mili():
+    km = (float(input(Fore.GREEN+"Give km value: ")))
+    mili = km*1000000
+    print(mili, "Mm")
+
+
+def cm_km():
+    cm = (float(input(Fore.GREEN+"Give cm value: ")))
+    km = cm/100000
+    print(km, "Km")
+
+
+def km_cm():
+    km = (float(input(Fore.GREEN+"Give km value: ")))
+    cm = km*100000
+    print(cm, "Cm")
+
+
+def metre_km():
+    metre = (float(input(Fore.GREEN+"Give m value: ")))
+    km = metre/1000
+    print(km, "Km")
+
+
+def km_metre():
+    km = (float(input(Fore.GREEN+"Give km value: ")))
+    metre = km*1000
+    print(metre)
+
+
+def mili_mile():
+    mili = (float(input(Fore.GREEN+"Give mm value: ")))
+    mile = mili/1609344
+    print(mile, "Miles")
+
+
+def mile_mili():
+    mile = (float(input(Fore.GREEN+"Give mile value: ")))
+    mili = mile*1609344
+    print(mili, "Mm")
+
+
+def cm_mile():
+    cm = (float(input(Fore.GREEN+"Give cm value: ")))
+    mile = cm/160934.4
+    print(mile, "Mile")
+
+
+def mile_cm():
+    mile = (float(input(Fore.GREEN+"Give mile value: ")))
+    cm = mile*160934.4
+    print(cm, "Cm")
+
+
+def metre_mile():
+    metre = (float(input(Fore.GREEN+"Give m value: ")))
+    mile = metre/1609.344
+    print(mile, "Mile")
+
+
+def mile_metre():
+    mile = (float(input(Fore.GREEN+"Give mile value: ")))
+    metre = mile*609.344
+    print(metre, "M")
+
+
+def km_mile():
+    km = (float(input(Fore.GREEN+"Give km value: ")))
+    mile = km/1.609344
+    print(mile, "Mile")
+
+
+def mile_km():
+    mile = (float(input(Fore.GREEN+"Give mile value: ")))
+    km = mile*1.609344
+    print(km, "Km")
+
+
+def mili_inch():
+    mili = (float(input(Fore.GREEN+"Give mm value: ")))
+    inch = mili/25.4
+    print(inch, "Inch")
+
+
+def inch_mili():
+    inch = (float(input(Fore.GREEN+"Give inch value: ")))
+    mili = inch*25.4
+    print(mili, "Mm")
+
+
+def cm_inch():
+    cm = (float(input(Fore.GREEN+"Give cm value: ")))
+    inch = cm/2.54
+    print(inch, "Inch")
+
+
+def inch_cm():
+    inch = (float(input(Fore.GREEN+"Give inch value: ")))
+    cm = inch*2.54
+    print(cm, "Cm")
+
+
+def metre_inch():
+    metre = (float(input(Fore.GREEN+"Give m value: ")))
+    inch = metre/00.254
+    print(inch, "Inch")
+
+
+def inch_metre():
+    inch = (float(input(Fore.GREEN+"Give inch value: ")))
+    metre = inch*00.254
+    print(metre, "M")
+
+
+def km_inch():
+    km = (float(input(Fore.GREEN+"Give km value: ")))
+    inch = km/0.0000254
+    print(inch, "Inch")
+
+
+def inch_km():
+    inch = (float(input(Fore.GREEN+"Give inch value: ")))
+    km = inch*0.0000254
+    print(km, "Km")
+
+
+def mile_inch():
+    mile = (float(input(Fore.GREEN+"Give mile value: ")))
+    inch = mile/63360
+    print(inch, "Inch")
+
+
+def inch_mile():
+    inch = (float(input(Fore.GREEN+"Give inch value: ")))
+    mile = inch*63360
+    print(mile, "Mile")
+
+
+def mili_foot():
+    mili = (float(input(Fore.GREEN+"Give mm value: ")))
+    foot = mili/304.8
+    print(foot, "Foot")
+
+
+def foot_mili():
+    foot = (float(input(Fore.GREEN+"Give foot value: ")))
+    mili = foot*304.8
+    print(mili, "Mm")
+
+
+def cm_foot():
+    cm = (float(input(Fore.GREEN+"Give cm value: ")))
+    foot = cm/30.48
+    print(foot, "Foot")
+
+
+def foot_cm():
+    foot = (float(input(Fore.GREEN+"Give foot value: ")))
+    cm = foot*30.48
+    print(cm, "Cm")
+
+
+def metre_foot():
+    metre = (float(input(Fore.GREEN+"Give m value: ")))
+    foot = metre/0.3048
+    print(foot, "Foot")
+
+
+def foot_metre():
+    foot = (float(input(Fore.GREEN+"Give foot value: ")))
+    metre = foot*0.3048
+    print(metre, "M")
+
+
+def km_foot():
+    km = (float(input(Fore.GREEN+"Give km value: ")))
+    foot = km/0.0003048
+    print(foot, "Foot")
+
+
+def foot_km():
+    foot = (float(input(Fore.GREEN+"Give foot value: ")))
+    km = foot*0.0003048
+    print(km, "Km")
+
+
+def inch_foot():
+    inch = (float(input(Fore.GREEN+"Give inch value: ")))
+    foot = inch/12
+    print(foot, "Foot")
+
+
+def foot_inch():
+    foot = (float(input(Fore.GREEN+"Give foot value: ")))
+    inch = foot*12
+    print(inch, "Inch")
+
+
+def mile_foot():
+    mile = (float(input(Fore.GREEN+"Give mile value: ")))
+    foot = mile/0.000189393939
+    print(foot, "Foot")
+
+
+def foot_mile():
+    foot = (float(input(Fore.GREEN+"Give foot value: ")))
+    mile = foot*0.000189393939
+    print(mile, "Mile")
