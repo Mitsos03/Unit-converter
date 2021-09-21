790fc92f23a808cebbe99e66b5de149901c0fa69
Statheros Dimitris <71936023+Mitsos03@users.noreply.github.com>
2021-09-21 10:40:25 +0300

    Create Time.py

---
+++ Formulas/Time.py
@@ -0,0 +1,178 @@
+def sec_minutes():
+    sec = (int(input(Fore.GREEN+"Give sec value: ")))
+    minutes = sec/60
+    print(minutes, "Minutes")
+
+
+def minutes_sec():
+    minutes = (int(input(Fore.GREEN+"Give min value: ")))
+    sec = minutes*60
+    print(sec, "secs")
+
+
+def sec_hour():
+    sec = (int(input(Fore.GREEN+"Give sec value: ")))
+    hour = sec/3600
+    print(hour, "Hours")
+
+
+def hour_sec():
+    hour = (int(input(Fore.GREEN+"Give hour value: ")))
+    sec = hour*3600
+    print(sec, "secs")
+
+
+def minutes_hour():
+    minutes = (int(input(Fore.GREEN+"Give min value: ")))
+    hour = minutes/60
+    print(hour, "Hours")
+
+
+def hour_minutes():
+    hour = (int(input(Fore.GREEN+"Give hour value: ")))
+    minutes = hour*60
+    print(minutes, "Minutes")
+
+
+def sec_day():
+    sec = (int(input(Fore.GREEN+"Give sec value: ")))
+    day = sec/86400
+    print(day, "Days")
+
+
+def day_sec():
+    day = (int(input(Fore.GREEN+"Give day value: ")))
+    sec = day*86400
+    print(sec, "Secs")
+
+
+def minutes_day():
+    minutes = (int(input(Fore.GREEN+"Give min value: ")))
+    day = minutes/1440
+    print(day, "Day")
+
+
+def day_minutes():
+    day = (int(input(Fore.GREEN+"Give day value: ")))
+    minutes = day*1440
+    print(minutes , "Minutes")
+
+
+def hour_day():
+    hour = (int(input(Fore.GREEN+"Give hour value: ")))
+    day = hour/24
+    print(day, "Days")
+
+
+def day_hour():
+    day = (int(input(Fore.GREEN+"Give day value: ")))
+    hour = day*24
+    print(hour, "Hours")
+
+
+def sec_week():
+    sec = (int(input(Fore.GREEN+"Give sec value: ")))
+    week = sec/604800
+    print(week, "Weeks")
+
+
+def week_sec():
+    week = (int(input(Fore.GREEN+"Give week value: ")))
+    sec = week*604800
+    print(sec, "Sec")
+
+
+def minute_week():
+    minute = (int(input(Fore.GREEN+"Give min value: ")))
+    week = minute*9.92063492*10**-5
+    print(week, "Weeks")
+
+
+def week_minute():
+    week = (int(input(Fore.GREEN+"Give week value: ")))
+    minute = week*1080
+    print(minute, "Minutes")
+
+
+def hour_week():
+    hour = (int(input(Fore.GREEN+"Give hour value: ")))
+    week = hour*0.00595238095
+    print(week , "Weeks")
+
+
+def week_hour():
+    week = (int(input(Fore.GREEN+"Give week value: ")))
+    hour = week/0.00595238095
+    print(hour, "Hours")
+
+
+def day_week():
+    day = (int(input(Fore.GREEN+"Give day value: ")))
+    week = day/7
+    print(week, "Weeks")
+
+
+def week_day():
+    week = (int(input(Fore.GREEN+"Give week value: ")))
+    day = week*7
+    print(day, "Days")
+
+
+def sec_year():
+    sec = (int(input(Fore.GREEN+"Give sec value: ")))
+    year = sec/31536000
+    print(year, "Years")
+
+
+def year_sec():
+    year = (int(input(Fore.GREEN+"Give year value: ")))
+    sec = year*31536000
+    print(sec, "Sec")
+
+
+def minute_year():
+    minute = (int(input(Fore.GREEN+"Give min value: ")))
+    year = minute*1.90132588*10**-6
+    print(year, "Years")
+
+
+def year_minute():
+    year = (int(input(Fore.GREEN+"Give year value: ")))
+    minute = year*525600
+    print(minute, "Minutes")
+
+
+def hour_year():
+    hour = (int(input(Fore.GREEN+"Give hour value: ")))
+    year = hour/8760
+    print(year, "Years")
+
+
+def year_hour():
+    year = (int(input(Fore.GREEN+"Give year value: ")))
+    hour = year*8760
+    print(hour, "Hours")
+
+
+def day_year():
+    day = (int(input(Fore.GREEN+"Give day value: ")))
+    year = day/365
+    print(year, "Years")
+
+
+def year_day():
+    year = (int(input(Fore.GREEN+"Give year value: ")))
+    day = year*365
+    print(day, "Days")
+
+
+def week_year():
+    week = (int(input(Fore.GREEN+"Give week value: ")))
+    year = week/52
+    print(year, "Years")
+
+
+def year_week():
+    year = (int(input(Fore.GREEN+"Give year value: ")))
+    week = year*52
+    print(week, "Weeks")
